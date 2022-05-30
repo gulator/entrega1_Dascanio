ad190331f59c7f794b8944a4ef205f508645469d
Julian Dascanio <julian.dascanio@gmail.com>
2022-05-30 03:44:29 -0300

    Entrega1 Dascanio V2

--- AppPeliculas/admin.py
+++ AppPeliculas/admin.py
@@ -4,6 +4,5 @@ from .models import *
 # Register your models here.
 admin.site.register(Pelicula)
 admin.site.register(Serie)
-admin.site.register(Usuario)
 admin.site.register(Genero)
 admin.site.register(Resenia)
